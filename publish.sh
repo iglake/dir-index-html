f1ef6cc8003d01451f70b21846a14c2303a157dc
Iggy Lake <iglake@yandex.com>
2019-07-24 18:57:29 +0200

    add cdn,cf,dwev,ipdf version of the dir-index.html

---
+++ publish.sh
@@ -0,0 +1,27 @@
+#
+
+rm -f *.org */*.org
+tic=$(date +%s)
+# ----------------------------------------------------------------------------------------------------------------
+qm=$(ipfs add -Q -w -r README.md dir-index.html dir-index-uncat.html gw-assets index.go knownIcons.txt LICENSE)
+bafy=$(ipfs cid base32 $qm)
+url="https://$bafy.cf-ipfs.com"
+sed -e "s,/ipns/Qm[^/]*,https://$bafy.cf-ipfs.com/gw-assets,g" dir-index-uncat.html > dir-index-cf.html
+sed -e "s,/ipns/Qm[^/]*,http://$bafy.ipfs.dweb.link/gw-assets,g" dir-index-uncat.html > dir-index-dweb.html
+sed -e "s,/ipns/Qm[^/]*,/ipfs/$qm/gw-assets,g" dir-index-uncat.html > dir-index-ipfs.html
+url=http://127.0.0.1:8080/ipfs/$(ipfs add -Q -n dir-index-ipfs.html)
+echo firefox $url
+# ----------------------------------------------------------------------------------------------------------------
+qm=$(ipfs add -Q -w -r README.md gw-assets dir-index.html dir-index-*.html index.go knownIcons.txt LICENSE)
+tag=$(git describe --abbrev=0 --tags)
+rm -f .gx/lastpubver.prv
+mv .gx/lastpubver .gx/lastpubver.prv
+echo $tag: $qm
+echo $tag: $qm > .gx/lastpubver
+# ----------------------------------------------------------------------------------------------------------------
+qm=$(ipfs add -Q -w -r gw-assets dir-index.html dir-index-dweb.html .gx qm.log)
+echo $tic: $qm >> qm.log
+echo http://$(ipfs cid base32 $qm).cf-ipfs.com/
+echo http://yoogle.com:8080/ipfs/$qm
+ipfs name publish --key=gw-assets /ipfs/$qm/gw-assets &
+# ----------------------------------------------------------------------------------------------------------------
