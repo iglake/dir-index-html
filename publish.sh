3131827eaea70b212f9b510077155adb3a19d2de
Iggy Lake <iglake@yandex.com>
2019-08-12 17:55:18 +0200

    publishing on 1565625318 for v1.3.8

--- publish.sh
+++ publish.sh
@@ -1,11 +1,14 @@
 #
 
 find . -name \*.org -delete
+find . -name \*~ -delete
 tic=$(date +%s)
 # ----------------------------------------------------------------------------------------------------------------
-qm=$(ipfs add -Q -r dir-index.html gw-assets --cid-version=1 --cid-base=base58btc)
+ver=$($HOME/bin/version dir-index.html | xyml scheduled)
+qm=$(ipfs add -Q -r dir-index.html gw-assets gq-assets/favicon.ico --cid-version=1 --cid-base=base58btc)
 sed -e "s,/ipfs/[bz][^/]*,/ipfs/$qm,g" dir-index.html > dir-index.html~
-mv -f dir-index.html~ dir-index.html
+sed -e "s,/dir-index-html/[^/]*/gw-assets/,/dir-index-html/$ver/gw-assets/,g" dir-index.html~ > dir-index.html
+#v -f dir-index.html~ dir-index.html
 # ----------------------------------------------------------------------------------------------------------------
 qm=$(ipfs add -Q -w --cid-version=1 --cid-base=base58btc -r gw-assets/favicon.ico README.md dir-index.html dir-index-uncat.html gw-assets index.go knownIcons.txt LICENSE)
 bafy=$(ipfs cid base32 $qm)
@@ -16,10 +19,12 @@ sed -e "s,/ipns/Qm[^/]*,/ipfs/$qm/gw-assets,g" dir-index-uncat.html > dir-index-
 url=http://127.0.0.1:8080/ipfs/$(ipfs add -Q -n dir-index-ipfs.html)
 echo firefox $url
 git add README.md dir-index.html dir-index-*.html gw-assets index.go knownIcons.txt LICENSE revs.log
-if git commit -m "publishing on $tic"; then
+if git commit -m "publishing on $tic for $ver"; then
 gitid=$(git rev-parse HEAD)
-echo gitid: $gitid
-echo $tic: $gitid >> revs.log
+git tag -f -a $ver -m "tagging $gitid on $(date +%D)"
+echo "gitid: $gitid ($ver)"
+echo "$tic: $gitid ($ver)" >> revs.log
+find .git/objects -type f -exec ipfs dag put --format=git --input-enc=zlib {} \; > hashes
 fi
 # ----------------------------------------------------------------------------------------------------------------
 qm=$(ipfs add -Q -w -r README.md gw-assets dir-index.html dir-index-*.html gw-assets/favicon.ico index.go knownIcons.txt LICENSE)
@@ -35,7 +40,9 @@ echo http://$(ipfs cid base32 $qm).cf-ipfs.com/
 echo http://yoogle.com:8080/ipfs/$qm
 ipfs name publish --key=gw-assets /ipfs/$qm/gw-assets &
 # ----------------------------------------------------------------------------------------------------------------
-cd ..
+if [ "x$GOPATH" != 'x' ]; then
+cd $GOPATH/src/github.com/ipfs/go-ipfs/assets
 go-bindata  -pkg=assets init-doc dir-index-html
 gofmt -w bindata.go
+fi
 
